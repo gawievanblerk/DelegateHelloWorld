aaa5396ca3909b81e8d8a0ca9be3da810bb32783
Gawie van Blerk <Gawie.VanBlerk@absa.africa>
2019-06-10 08:05:28 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace DelegateHelloWorld
+{
+    class Program
+    {
+        static bool isAuth;
+
+        delegate void SayHelloDelegate(string name);
+        public static void SayHello(string name)
+        {
+            if (!isAuth)
+            {
+                Console.WriteLine("Not Authorized.");
+                isAuth = true;
+                SayHelloDelegate del = new SayHelloDelegate(SayHello);
+                del(name);
+            }
+            else
+            {
+                Console.WriteLine("SayHello is now Authorized.");
+                Console.WriteLine("Hello {0}", name);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SayHello("World");
+            Console.ReadLine();
+        }
+    }
+}
